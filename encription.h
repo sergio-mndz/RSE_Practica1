67eeff4a7613ec195a690d54e1e2b92a2258ad21
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2021-09-18 10:40:19 -0500

    Created encription header file

---
+++ encription.h
@@ -0,0 +1,29 @@
+/*
+ * encription.h
+ *
+ *  Created on: 18 sep 2021
+ *      Author: sergio_mndz
+ */
+
+#ifndef RSE_PRACTICA1_ENCRIPTION_H_
+#define RSE_PRACTICA1_ENCRIPTION_H_
+
+#include "aes.h"
+#include "tcpecho.h"
+#include "lwip/netifapi.h"
+#include "lwip/tcpip.h"
+#include "netif/ethernet.h"
+#include "enet_ethernetif.h"
+
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+#include "fsl_phy.h"
+
+#include "fsl_phyksz8081.h"
+#include "fsl_enet_mdio.h"
+#include "fsl_device_registers.h"
+
+
+
+#endif /* RSE_PRACTICA1_ENCRIPTION_H_ */
