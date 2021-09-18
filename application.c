06b3f61d539e1f6faea046bdf488c8bd702d75ad
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2021-09-18 11:22:23 -0500

    Added application layer files

---
+++ application.c
@@ -0,0 +1,8 @@
+/*
+ * application.c
+ *
+ *  Created on: 18 sep 2021
+ *      Author: sergio_mndz
+ */
+
+#include "application.h"
---
+++ application.h
@@ -0,0 +1,13 @@
+/*
+ * application.h
+ *
+ *  Created on: 18 sep 2021
+ *      Author: sergio_mndz
+ */
+
+#ifndef RSE_PRACTICA1_APPLICATION_H_
+#define RSE_PRACTICA1_APPLICATION_H_
+
+#include "aes.h"
+
+#endif /* RSE_PRACTICA1_APPLICATION_H_ */
