5c57f5ed5e8193b79b05635c24cf80d0b28894de
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2021-09-18 10:53:55 -0500

    add c file of encryption

---
+++ encription.c
@@ -0,0 +1,18 @@
+/*
+ * encription.c
+ *
+ *  Created on: 18 sep 2021
+ *      Author: sergio_mndz
+ */
+
+#include "encription.h"
+
+const char* encrypt_message(uint8_t* buf)
+{
+
+}
+
+const char* decrypt_message(uint8_t* buf)
+{
+
+}
